fd4b862dcca47590027012e8444d43f08b30e80c
Diego Aguilef <diego.aguilef@gmail.com>
2018-04-17 14:57:45 -0300

    chore(login): migrates ddbb to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180413125051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
