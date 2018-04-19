817f2966b7f6e9fc0e4378a83394177cdf95dc5e
Diego Aguilef <diego.aguilef@gmail.com>
2018-04-19 20:44:16 -0300

    chore(login): adds input mask plugin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,27 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+	attr_accessor :login
+	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
+  	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+  	validate :validate_username
 	validates :name, presence: true, on: [:update]
 	validates :age, numericality: true, on: [:update]
 	validates :birth_date, presence: true, on: [:update]
 	validates :cell_phone, numericality: true, on: [:update]
 
+	def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+
+    def validate_username
+  		if User.where(email: username).exists?
+    		errors.add(:username, :invalid)
+  		end
+	end
 end
