bc2ee74a3227b93d269bf0498ea380e3d2fc4e41
Diego Aguilef <diego.aguilef@gmail.com>
2018-04-28 19:16:18 -0300

    chore(roles): migrates role tables & config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
 	attr_accessor :login
 	# Include default devise modules. Others available are:
   	# :confirmable, :lockable, :timeoutable and :omniauthable
