2b28af9acc25e66c894853e27c113dabf3b19afb
Diego Aguilef <diego.aguilef@gmail.com>
2018-05-08 14:39:57 -0300

    adds random page to home

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'home/index'
   get 'home/chat'
+  get 'home/random'
   root to: 'home#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
